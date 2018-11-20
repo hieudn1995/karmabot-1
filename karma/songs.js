e1afe78e86b1ff5dfa66f8004b2aecb8d70818cb
sageexplorer <srimal@campusexplorer.com>
2018-11-20 15:29:42 -0800

    Update songs.js

--- karma/songs.js
+++ karma/songs.js
@@ -15,9 +15,7 @@ app.post('/karma', function (req, res) {
 
   res.send("_checking_ _karma_..")
   console.log("WRITING KARMA POINTS:")
-  // sanitize and get the result, and put in the database
   let karma     = req.body.text
-  //let karma     = '/karma @sage++'
   let res_url   = req.body.response_url
   let user_name = req.body.user_name
   let name      = /(?:@)[^+-\s]+/gm.exec(karma)|| '@help'
@@ -33,9 +31,7 @@ app.post('/karma', function (req, res) {
   // Write to slack
   // always returns a karma point
   // If positive count or negative count is null, then query the total karma points
-  //(posCount !== 0 || negCount !== 0)
-
-  if (points != 0 || points != '0'){
+   if (points != 0 || points != '0'){
     //add/delete points
     console.log("my name is", addName)
     console.log("my point is", points)
@@ -48,11 +44,8 @@ app.post('/karma', function (req, res) {
       await delay(200)
       await querySongs(addName,0,user_name, res_url)
   }
-
   addKarma();
-
   }
-
   else if (req.body.text.indexOf('help') >= 0)  {
     // If help is asked, no need to call any other method, or do a calc
    this.karma('thisKarma','help', 'help', res_url);
@@ -64,11 +57,9 @@ app.post('/karma', function (req, res) {
   else {
     //If ++ or -- is not mentioned then it is useless to add/delete so simply return the value
     querySongs(addName,0, undefined, res_url);
-    console.log('I AM IN THE RIGHT PLACE')
   }
 
 }) // close post method
-
 
 app.listen(port, function () {
   console.log('Listening on port ' + port);

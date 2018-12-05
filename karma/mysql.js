4e68e97535e6664cf5cdd73b5fce64e61011aa2a
Max IMBERT <maximbert@gmail.com>
2018-12-05 11:01:37 -0800

    linting

--- karma/mysql.js
+++ karma/mysql.js
@@ -1,47 +1,46 @@
-require('dotenv').config({path:`${__dirname}/.env`});
-let mysql     = require('mysql');
-let system    = require('system');
-let newKarma  = require('./karma')
+require('dotenv').config({path: `${__dirname}/.env`});
+const mysql = require('mysql');
+const system = require('system');
+const newKarma = require('./karma');
 
+querySongs = function (userId, points, user_name, res_url, emoji) {
+	const Initialized = false;
+	const con = mysql.createConnection({
+		multipleStatements: true,
+		host: process.env.HOST,
+		user: process.env.DB_USER,
+		password: process.env.DB_PASSWORD,
+		port: 3306,
+		database: process.env.DATABASE
+	});
 
-querySongs = function(userId, points, user_name, res_url, emoji) {
-  let Initialized = false;
-  let con         = mysql.createConnection({
-    multipleStatements: true,
-    host:      process.env.HOST,
-    user:      process.env.DB_USER,
-    password:  process.env.DB_PASSWORD,
-    port: 3306,
-    database:  process.env.DATABASE
-  });
+	con.connect(err => {
+		console.log('Connected!');
+		console.log('my userID is', userId);
+		console.log('my DBpoint is', points);
+		console.log('my host is', process.env.HOST);
+		if (points != 0 || points != '0') {
+			const post = {name: userId, points};
+			con.query('INSERT INTO karma SET ?', post, (err, result) => {
+				if (err) {
+					throw err;
+				}
+				con.end();
+			});
+		} else {
+			con.query('select SUM(points) as total from karma where name= ?', [userId], (err, result) => {
+				if (err) {
+					throw err;
+				}
+				const queryResult = result[0];
+				console.log(queryResult.total);
 
-  con.connect(function(err) {
-    console.log("Connected!");
-    console.log("my userID is", userId)
-    console.log("my DBpoint is", points)
-    console.log("my host is", process.env.HOST)
-    if (points != 0 || points != '0') {
-      let post  = {name: userId, points: points};
-      con.query('INSERT INTO karma SET ?', post, function (err, result) {
-        if (err) throw err;
-        con.end();
-      });
-    }
-    else {
-      con.query("select SUM(points) as total from karma where name= ?",[userId], function (err, result) {
-        if (err) throw err;
-        let queryResult = result[0]
-        console.log(queryResult.total)
-
-        //CALL SLACK FROM HERE -->
-        karma(queryResult.total, userId, user_name, res_url, emoji)
-        con.end();
-      });
-    }
-
-  });
-
-}
-
+				// CALL SLACK FROM HERE -->
+				karma(queryResult.total, userId, user_name, res_url, emoji);
+				con.end();
+			});
+		}
+	});
+};
 
 module.exports = querySongs();
